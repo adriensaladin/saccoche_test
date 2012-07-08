6e44223d27d5c7e0dc5deea86ef53c012dce4d74
thomas <thomas@3af1deee-4ace-4d6f-927a-55d690a2d960>
2012-07-08 07:22:58 +0000

    Correction bug affichage bulletin HTML et non minification css/js depuis la version précédente.

--- trunk/_inc/code_items_synthese.php
+++ trunk/_inc/code_items_synthese.php
@@ -320,6 +320,8 @@ if( ($make_html) || ($make_graph) )
 	$releve_HTML .= (!$make_graph) ? '<div class="astuce">Cliquer sur <img src="./_img/toggle_plus.gif" alt="+" /> / <img src="./_img/toggle_moins.gif" alt="+" /> pour afficher / masquer le détail'.$bouton_print_appr.'</div>' : '<div id="div_graphique"></div>' ;
 	$separation = (count($tab_eleve)>1) ? '<hr class="breakafter" />' : '' ;
 	$legende_html = ($legende=='oui') ? affich_legende_html( FALSE /*codes_notation*/ , TRUE /*etat_acquisition*/ , FALSE /*pourcentage_acquis*/ , FALSE /*etat_validation*/ ) : '' ;
+	$width_barre = (!$make_officiel) ? 180 : 50 ;
+	$width_texte = 900 - $width_barre;
 }
 if($make_pdf)
 {
@@ -407,8 +409,8 @@ foreach($tab_eleve as $tab)
 							if($make_html)
 							{
 								$releve_HTML .= '<tr>';
-								$releve_HTML .= (!$make_officiel) ? affich_barre_synthese_html($width=180,$tab_infos_synthese,$total) : affich_pourcentage_html( 'td' , $tab_infos_synthese , FALSE /*detail*/ , 50 /*largeur*/ ) ;
-								$releve_HTML .= (!$make_officiel) ? '<td style="width:720px">' : '<td style="width:850px">' ;
+								$releve_HTML .= affich_barre_synthese_html($width_barre,$tab_infos_synthese,$total);
+								$releve_HTML .= '<td style="width:'.$width_texte.'px">' ;
 								$releve_HTML .= '<a href="#" id="to_'.$synthese_ref.'_'.$eleve_id.'"><img src="./_img/'.$toggle_img.'.gif" alt="" title="Voir / masquer le détail des items associés." class="toggle" /></a> ';
 								$releve_HTML .= html($tab_synthese[$synthese_ref]);
 								$releve_HTML .= '<div id="'.$synthese_ref.'_'.$eleve_id.'"'.$toggle_class.'>'.implode('<br />',$tab_infos_detail_synthese[$eleve_id][$synthese_ref]).'</div>';

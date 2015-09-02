2d1868d12fb51f6ad3e27e054a85e4512ea91bfc
thomas <thomas@3af1deee-4ace-4d6f-927a-55d690a2d960>
2015-09-02 20:15:49 +0000

    + Prise en compte du nouveau format d'export de ItsLearning (sauf parents). + Ajout de codes couleur. + Ajout d'index sur quelques tables de la base de données afin d'améliorer certaines requêtes.

--- _sql/structure/sacoche_jointure_message_destinataire.sql
+++ _sql/structure/sacoche_jointure_message_destinataire.sql
@@ -6,5 +6,7 @@ CREATE TABLE IF NOT EXISTS sacoche_jointure_message_destinataire (
   destinataire_type ENUM("all","niveau","classe","groupe","besoin","user") COLLATE utf8_unicode_ci NOT NULL DEFAULT "user",
   destinataire_id   MEDIUMINT(8)                                           UNSIGNED                NOT NULL DEFAULT 0,
   PRIMARY KEY ( message_id , user_profil_type , destinataire_type , destinataire_id ),
-  KEY message_id (message_id)
+  KEY destinataire ( destinataire_type , destinataire_id ),
+  KEY message_id (message_id),
+  KEY user_profil_type (user_profil_type)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;

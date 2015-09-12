78d7f4905cb1e74f19475d35e9a161269f49243c
thomas <thomas@3af1deee-4ace-4d6f-927a-55d690a2d960>
2015-09-12 18:51:06 +0000

    + Pour le cartouche associé à une évaluation, ajout de l'indication de la classe ou du groupe à côté du nom de l'élève. + Correction d'un souci de non pré-sélection du niveau lors de la modification d'une classe ou d'un groupe.

--- _inc/code_officiel_consulter.php
+++ _inc/code_officiel_consulter.php
@@ -262,6 +262,7 @@ if($BILAN_TYPE=='releve')
 elseif($BILAN_TYPE=='bulletin')
 {
   $synthese_modele = 'multimatiere' ;
+  $matiere_nom     = '';
   $groupe_id       = (!$is_sous_groupe) ? $classe_id  : $groupe_id ; // Le groupe  = la classe (par défaut) ou le groupe transmis
   $groupe_nom      = $groupe_nom; // Déjà défini avant car on en avait besoin
   $groupe_type     = (!$is_sous_groupe) ? 'Classe'  : 'Groupe' ;

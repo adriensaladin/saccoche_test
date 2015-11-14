6c4afa0a8b335417050075b3273931ebefcc3c86
thomas <thomas@3af1deee-4ace-4d6f-927a-55d690a2d960>
2015-11-14 13:23:28 +0000

    Correctif d'un message ne disparaissant pas lors de la mise à jour par le webmestre.

--- trunk/pages/webmestre_maintenance.js
+++ trunk/pages/webmestre_maintenance.js
@@ -189,7 +189,7 @@ $(document).ready
       }
       else
       {
-        $('#ajax_maj').removeAttr('class').addClass('loader').html('Etape '+etape_numero+' - '+etape_info);
+        $('#ajax_maj').removeAttr('class').addClass('loader').html('Etape '+etape_numero+' - '+etape_info['value']);
         $.ajax
         (
           {
@@ -213,7 +213,7 @@ $(document).ready
               }
               else
               {
-                maj_etape(responseJSON['value']);
+                maj_etape(responseJSON);
               }
             }
           }

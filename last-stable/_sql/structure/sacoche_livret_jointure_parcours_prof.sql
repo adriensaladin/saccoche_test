c67b9d61fae330dc6a465cb51d491da9d632a192
dcaillibaud <dcaillibaud@3af1deee-4ace-4d6f-927a-55d690a2d960>
2017-03-31 22:39:33 +0000

    on revient en version 1570, avant ajout du dossier last-stable

--- last-stable/_sql/structure/sacoche_livret_jointure_parcours_prof.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS sacoche_livret_jointure_parcours_prof;
-
-CREATE TABLE sacoche_livret_jointure_parcours_prof (
-  livret_parcours_id SMALLINT(5) UNSIGNED NOT NULL DEFAULT 0,
-  prof_id           MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT 0,
-  PRIMARY KEY ( livret_parcours_id , prof_id ),
-  KEY prof_id (prof_id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
